cb632ca9fe3215400c806a1431e910bf6131462a
mopstream <mitrandir_gendalf@mail.ru>
2024-10-17 13:57:45 +0300

    add tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,22 @@
+import qualified Data.List as L
+import Lib
+import Test.QuickCheck
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  putStrLn "Cheking linear interpolation ..."
+  quickCheck (withMaxSuccess 1000 propLinId)
+  putStrLn "Cheking lagrangia interpolation ..."
+  quickCheck (withMaxSuccess 1000 propLagId)
+
+propLinId :: [Double] -> [Double] -> Bool
+propLinId xs ys = and $ zipWith (\a b -> abs (a - b) < 0.00001) (linInter points x) y
+  where
+    points = L.nubBy (\a b -> fst a == fst b) (zip (L.sort xs) ys)
+    (x, y) = unzip points
+
+propLagId :: [Double] -> [Double] -> Bool
+propLagId xs ys = and $ zipWith (\a b -> abs (a - b) < 0.00001) (lagInter points x) y
+  where
+    points = L.nubBy (\a b -> fst a == fst b) (zip (L.sort xs) ys)
+    (x, y) = unzip points
